7565712153554b166acb6e36557a12b1c7651d7c
giansingh4710 <giansingh4710@gmail.com>
2023-04-12 19:26:04 +0000

    site live. But won't work using github pages.

--- getText.php
+++ getText.php
@@ -4,5 +4,13 @@ header("Access-Control-Allow-Origin: *");
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-echo readfile("theText.txt");
+$fileName = "theText.txt";
+
+$myfile = fopen($fileName, "r");
+if (filesize($fileName) == 0 ){
+  fclose($myfile);
+  die();
+}
+echo fread($myfile,filesize($fileName));
+fclose($myfile);
 ?>
--- saveText.php
+++ saveText.php
@@ -1,12 +1,12 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
 
-echo "<pre>";
-var_dump($_POST);
-echo "</pre>";
+//echo "<pre>";
+//var_dump($_POST);
+//echo "</pre>";
 
 $fp = fopen("theText.txt", "w"); 
 fwrite($fp,$_POST["theText"]);
