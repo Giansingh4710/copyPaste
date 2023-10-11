fe559d334712482784d1b5c907e68434285d4656
giansingh4710 <giansingh4710@gmail.com>
2023-10-11 18:34:04 +0000

    fixed error. issue was theText.txt needed write permisions

--- saveText.php
+++ saveText.php
@@ -8,8 +8,16 @@ header("Access-Control-Allow-Origin: *");
 //var_dump($_POST);
 //echo "</pre>";
 
-$fp = fopen("theText.txt", "w"); 
-fwrite($fp,$_POST["theText"]);
+$fp = fopen("./theText.txt", "w"); 
+if ($fp === false) {
+    die("Failed to open the file for writing.");
+}
+
+//ob_flush();
+if (fwrite($fp, $_POST["theText"]) === false) {
+    die("Failed to write to the file.");
+}
 fclose($fp); 
+
 header("Location: ./index.html");
 ?>
